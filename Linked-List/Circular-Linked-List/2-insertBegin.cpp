cff9e7007389dfba04f02642208df5ecbaa3c3de
Vasu Singh <85363195+vasucp1207@users.noreply.github.com>
2021-11-07 18:24:06 +0530

    Create 2-insertBegin.cpp

---
+++ Linked-List/Circular-Linked-List/2-insertBegin.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct node{
+	int data;
+	node* next;
+	
+	node(int x){
+		data = x;
+		next = NULL;
+	}
+};
+
+void display(node* head){
+	
+	if(head == NULL) return ;
+	
+	node* curr = head;
+	cout << curr->data << " ";       // curr->data == head->data
+	curr = curr->next;             // curr->data != head->data
+	
+	while(curr->data != head->data){
+		cout << curr->data << " ";
+		curr = curr->next;
+	}
+}
+
+node* insertBegin(node* head, int x){
+	
+	node* temp = new node(x);
+	if(head == NULL){
+		temp->next = temp;
+		return temp;
+	}
+	else{
+		
+		temp->next = head->next;
+		head->next = temp;
+		int t = head->data;
+		head->data = temp->data;
+		temp->data = t;
+		return head;
+	}
+}
+
+int main(){
+	
+	node* head = new node(10);
+	node* temp1 = new node(20);
+	node* temp2 = new node(30);
+	
+	head->next = temp1;
+	temp1->next = temp2;
+	temp2->next = head;
+	
+	head = insertBegin(head, 5);
+	display(head);
+	
+	
+
+return 0;
+}
